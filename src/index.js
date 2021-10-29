b201f5721b2b35884d039cc7639c25385f23ab0a
unknown <janruva@gmail.com>
2021-10-29 18:25:25 -0500

    Se añade cambios al index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {GraphQLServer} from 'graphql-yoga'
-import {typeDefs} from './graphql/typeDefs.js'
-import {resolvers} from './graphql/resolvers.js' 
+import {typeDefs} from './graphql/typeDefs'
+import {resolvers} from './graphql/resolvers' 
 
 const server = new GraphQLServer({
     typeDefs, 
