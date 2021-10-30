680a319f0713a44b4f6580691010c2ae6a283f25
unknown <janruva@gmail.com>
2021-10-29 21:00:01 -0500

    "Se crean las relaciones entre colecciones"

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,7 +1,46 @@
+import { books } from "../data/books"
+import { authors } from "../data/authors"
+import { users } from "../data/users"
+
 export const resolvers = {
-    Query:{
-        ping(){
+    Query: {
+        ping() {
             return "pong"
+        },
+        books() {
+            return books;
+        },
+        authors() {
+            return authors;
+        },
+        users() {
+            return users;
         }
-    } 
+    },
+    Book: {
+        author: ({ author }) => {
+            return authors.find(a => {
+                return a.id === author
+            })
+        },
+        users: (parent) => {
+            return users.filter(user =>{
+                return user.book === parent.id
+            })
+        }
+    },
+    Author: {
+        books(parent) {
+            return books.filter(book => {
+                return book.author === parent.id;
+            })
+        }
+    },
+    User: {
+        book (parent) {
+            return books.find(book =>{
+                return book.id === parent.book
+            })
+        }
+    }
 }
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,5 +1,24 @@
 export const typeDefs =  `
-    type Query{
+    type Query {
         ping: String!
+        books: [Book!]!
+        authors: [Author!]!
+        users: [User!]!
+    }
+    type Book{
+        id: ID!
+        title: String!
+        author: Author!
+        users: [User!]!
+    }
+    type Author {
+        id: ID!
+        name: String!
+        books: [Book!]!
+    }
+    type User{
+        id:ID!
+        name: String!
+        book: Book!
     }
 `;
